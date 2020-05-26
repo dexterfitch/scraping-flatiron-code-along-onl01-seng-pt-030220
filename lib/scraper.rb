57ab8fff79f03e5aeb5f10f2dcbe487cc5c1085a
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-26 21:51:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,10 +22,11 @@ class Scraper
   end
 
   def get_courses
+    self.get_page.css(".post")
   end
 
   def make_courses
-    doc.css(".post").each do |post|
+    self.get_courses(".post").each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
